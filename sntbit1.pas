34d2ac34a80fc901ae01bacd92fcb54bbbbbdb46
agjádfjhg <quandev2k7@gmail.com>
2022-05-26 21:21:33 +0700

    Pascal

--- sntbit1.pas
+++ sntbit1.pas
@@ -1,22 +1,36 @@
 const fi='bai3.inp';
-	  fo='bai3.out';
-var f,g:text;
-	n,h:longint;
+      fo='bai3.out';
+var n,h:longint;
+f,g:text;
 procedure nhap;
 begin
 	assign(f,fi);reset(f);
 	assign(g,fo);rewrite(g);
-	read(f,n); read(f,h); 
+	readln(f,n,h);
 end;
-
-procedure xuli;
-var i,l:longint;
+function kt(n:longint):boolean;
+var i:longint;
 begin
-for i:=1 to n do  
+	if n<2 then exit(false);
+	for i:=2 to trunc(sqrt(n)) do
+	if n mod i = 0 then exit(false);
+	exit(true);
 end;
-begin 
+procedure tim;
+var i,du,d:longint;   s:string;
+begin
+	d:=0;
+	s:='';
+	while n<>0 do
+	begin
+	du:=n mod 2;
+	n:=n div 2;
+	inc(D);
+	end;
+end;
+begin
 nhap;
-close(f);
-close(g);
+tim;
+close(F);
+close(G);
 end.
-	
