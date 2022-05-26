98357efe6b887644d331c317ab17f1d1eee67575
agjádfjhg <quandev2k7@gmail.com>
2022-05-26 18:31:20 +0700

    Pascal

--- bai3detinh3.pas
+++ bai3detinh3.pas
@@ -1,42 +1,42 @@
 const fi='bai3.inp';
-      fo='bai3.out';
+			fo='bai3.out';
 var A,F:array[0..2*100000]of Qword;
-    N,K:longint;
-    f1,g:text;
+		N,K:longint;
+		f1,g:text;
 
 procedure taodayfibo(M:longint)
 begin
-  F[1]:=1;
-  F[2]:=1;
-  K:=2;
-  while (F[K]<=M) do
-  begi
-     K:=K+1;
-     F[K]:=F[K-2]+F[K-1];
-  end;
-  end;
+	F[1]:=1;
+	F[2]:=1;
+	K:=2;
+	while (F[K]<=M) do
+	begi
+		 K:=K+1;
+		 F[K]:=F[K-2]+F[K-1];
+	end;
+	end;
 
 procedure xuLi;
 Var i,j:longint;
 begin
-  j:=0;
-  for i:=k downto 1 do
-  if F[i]<=N then begin inc(j);A[j]:=F[i];
+	j:=0;
+	for i:=k downto 1 do
+	if F[i]<=N then begin inc(j);A[j]:=F[i];
  N:=N-F[i];end;
-  if N=0 then for i:=1 to j do write(g,A[i],' ')
-  else write(g,'0');
+	if N=0 then for i:=1 to j do write(g,A[i],' ')
+	else write(g,'0');
 end;
 
 begin
-  assign(f1,fi);reset(f1);
-  assign(g,fo);rewrite(g)
-  readln(f1,n);
+	assign(f1,fi);reset(f1);
+	assign(g,fo);rewrite(g)
+	readln(f1,n);
 end;
 
 begin
-  nhap;
-  taodayfibo(N);
-  xuli;
-  close(f1);
-  close(g);
+	nhap;
+	taodayfibo(N);
+	xuli;
+	close(f1);
+	close(g);
 end.
