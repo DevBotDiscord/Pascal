5ea44ce9b15688c5457c248906320a2251e10d32
DevBotDiscord <102542986+DevBotDiscord@users.noreply.github.com>
2022-05-30 21:42:40 +0700

    Pascal

--- bai3.pas
+++ bai3.pas
@@ -1,30 +1,30 @@
 var s: string;
-        j:byte;
+	j:byte;
 function mahoa(a:char): char;
-        var t: array[0..25] of char;
-            ch: char;
-            x,i: byte;
-        begin
-                i:=0;
-                for ch:='a' to 'z' do
-                        begin
-                        t[i]:=ch;
-                        inc(i);
-                        end;
-                for i:=0 to 25 do
-                        if t[i]=a then
-                                begin
-                                x:=i;
-                                break;
-                                end;
-                i:=(x+5) mod 26;
-                mahoa:= t[i];
-        end;
+		var t: array[0..25] of char;
+			ch: char;
+			x,i: byte;
+		begin
+				i:=0;
+				for ch:='a' to 'z' do
+						begin
+						t[i]:=ch;
+						inc(i);
+						end;
+				for i:=0 to 25 do
+						if t[i]=a then
+								begin
+								x:=i;
+								break;
+								end;
+				i:=(x+5) mod 26;
+				mahoa:= t[i];
+		end;
 begin
-        write(' Nhap xau can ma hoa: ');
-        readln(s);
-        writeln(' Xau ma hoa la:');
-        for j:=1 to length(s) do
-                write(mahoa(s[j]));
-        readln
+		write(' Nhap xau can ma hoa: ');
+		readln(s);
+		writeln(' Xau ma hoa la:');
+		for j:=1 to length(s) do
+				write(mahoa(s[j]));
+		readln
 end.
