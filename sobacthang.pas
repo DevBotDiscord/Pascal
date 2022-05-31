e4ad3793dd9d60df8e4ad7cee753293221777d52
DevBotDiscord <102542986+DevBotDiscord@users.noreply.github.com>
2022-05-31 08:37:18 +0700

    Pascal

--- sobacthang.pas
+++ sobacthang.pas
@@ -1,33 +1,34 @@
 const fi='bai4.inp';
       fo='bai4.out';
-Var i,n1,n2: integer;
+Var i,n,n1,n2:longint;
     f,g:text;
 
-Function kt(n:integer):Boolean;
-Var ok: boolean;
-    so:byte;
+function kt(n:integer):Boolean;
+Var so:byte;
 Begin
-    ok:=true;
     While n>=10 do
     Begin
     so:=n mod 10;
     n:=n div 10;
-    if so < (n mod 10) then ok:=ok and false;
+    if so < (n mod 10) then exit(false);
     End;
-    kt:=ok;
+    exit(true);
 End;
 procedure main;
 begin 
     For i:= n1 to n2 do if kt(i) then Write(g,i:4);
 end;
 procedure nhap;
+var j:longint;
 begin 
     assign(f,fi);reset(f);
     assign(g,fo);rewrite(g);
-    while not eof(f) do 
-    begin
-    readln(f,n1,n2);
+    readln(f,n);
+    for j:=1 to n do 
+    begin 
+    read(f,n1,n2);
     main();
+    writeln(g);
     end;
 end;
 Begin
