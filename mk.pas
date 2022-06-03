e5f383460d3acc80f157739c7a0272ba8bcc67a8
unknown <quandev2k7@gmail.com>
2022-06-03 07:09:48 +0700

    Pascal

---
+++ mk.pas
@@ -0,0 +1,16 @@
+const fi='bai1.inp';
+      fo='bai1.out';
+     var n,k:longint;
+           f,g:text;
+procedure nhap;
+	begin
+		assign(f,fi);reset(f);
+		assign(g,fo);rewrite(g);
+		readln(f,n,k);		
+	end;
+procedure tim;
+	var i,d:longint;
+begin
+	for i:=n downto 1 do 
+
+end
