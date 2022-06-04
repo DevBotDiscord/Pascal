4d782554c81c7c7a0f4450ce3ce0a927949c5328
unknown <quandev2k7@gmail.com>
2022-06-04 19:52:48 +0700

    Pascal

--- bai2de7.pas
+++ bai2de7.pas
@@ -9,6 +9,21 @@ begin
 	readln(f,n);
 end;
 procedure xuli;
+var l,i,a,t:longint;
 begin 
-
+for l:=1 to n div 2 do 
+	if ((2*n-l*(l+1)) mod (2*(l+1)) =0) and (2*n-l*(l+1)>0) then
+	begin 
+	t:=l;
+	a:=(2*n-l*(l+1)) div (2*(l+1));
+	end;
+for i:=a to a+t do
+write(g,i,' ');
+writeln(g); 
 end;
+begin 
+	nhap;
+	xuli;
+	close(f);
+	close(g);
+end.
