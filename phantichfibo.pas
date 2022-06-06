dd1dd7fc1362bbca85ad88d8b65297ec48efaeb6
unknown <quandev2k7@gmail.com>
2022-06-06 16:30:47 +0700

    Pascal

--- phantichfibo.pas
+++ phantichfibo.pas
@@ -11,41 +11,45 @@ Begin
    read(f,n);
 end;
 Procedure Xuli;
-Var Fibonacy, vitri: array[1..100] of integer;
+Var fibo,vt: array[1..100] of integer;
    t, i, d, j: integer;
    ok: boolean;
 Begin
-   Fibonacy[1]:= 1;
-   Fibonacy[2]:= 1;
-   t:= 1;
-   d:= 2;
-   ok:= true;
-   While t < n do
+   fibo[1]:=1;
+   fibo[2]:=1;
+   t:=1;
+   d:=2;
+   ok:=true;
+   While t<n do
       begin
          inc(d);
-         Fibonacy[d]:= Fibonacy[d-1]+ Fibonacy[d-2];
-         t:= Fibonacy[d];
+         fibo[d]:=fibo[d-1]+fibo[d-2];
+         t:=fibo[d];
       end;
-   t:= n;
-   j:= 0;
-   For i:= d downto 1 do
+   t:=n;
+   j:=0;
+   For i:=d downto 1 do
       begin
-         If Fibonacy[i] <= t then
+         If fibo[i]<t then
             begin
                inc(j);
-               Vitri[j]:= i;
-               t:= t - Fibonacy[i];
+               vt[j]:=i;
+               t:=t-fibo[i];
             end;
       end;
-      If t <> 0 then ok:= false;
-      If Ok = true then
+      If t<>0 then ok:=false;
+      
+      If ok=true then
+      begin 
+      	write(g,n,'=');
             for i:= 1 to j do
-                                begin
-                                If i <> j then
-                                write(g, Fibonacy[vitri[i]], ' + ') else
-                                write(g, Fibonacy[vitri[i]])
-                                end
-                                else write(g, 'Khong the bieu dien ');
+              begin
+              If i <> j then
+              write(g, fibo[vt[i]], '+') else
+              write(g, fibo[vt[i]])
+              end
+      end
+              else write(g, 'Khong the bieu dien ');
 
 end;
 Begin
