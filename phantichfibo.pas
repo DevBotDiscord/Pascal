36a01bb968ec02f8ce56f8537f3fcb540c3aa948
unknown <quandev2k7@gmail.com>
2022-06-06 20:12:03 +0700

    Pascal

--- phantichfibo.pas
+++ phantichfibo.pas
@@ -1,60 +1,56 @@
 const fi='fibo.inp';
-      fo='fibo.out';
+		fo='fibo.out';
 Var f,g: text;
-    n: integer;
-Procedure nhap;
+	n: integer;
+procedure nhap;
 Begin
-   Assign(f,fi);
-   Reset(f);
-   assign(g,fo);
-   rewrite(g);
-   read(f,n);
+	assign(f,fi); reset(f);
+	assign(g,fo);rewrite(g);
+	readln(f,n);
 end;
-Procedure Xuli;
+procedure xuli;
 Var fibo,vt: array[1..100] of integer;
-   t, i, d, j: integer;
-   ok: boolean;
+	t,i,d,j: integer;
+	ok:boolean;
 Begin
-   fibo[1]:=1;
-   fibo[2]:=1;
-   t:=1;
-   d:=2;
-   ok:=true;
-   While t<n do
-      begin
-         inc(d);
-         fibo[d]:=fibo[d-1]+fibo[d-2];
-         t:=fibo[d];
-      end;
-   t:=n;
-   j:=0;
-   For i:=d downto 1 do
-      begin
-         If fibo[i]<t then
-            begin
-               inc(j);
-               vt[j]:=i;
-               t:=t-fibo[i];
-            end;
-      end;
-      If t<>0 then ok:=false;
-      
-      If ok=true then
-      begin 
-      		write(g,n,'=');
-            for i:= 1 to j do
-              begin
-              If i <> j then
-              write(g, fibo[vt[i]], '+') else
-              write(g, fibo[vt[i]])
-              end
-      end
-      else write(g, 'Khong the bieu dien ');
-
+	fibo[1]:= 1;
+	fibo[2]:= 1;
+	t:= 1;
+	d:= 2;
+	ok:= true;
+	While t < n do
+		begin
+			inc(d);
+			fibo[d]:= fibo[d-1]+fibo[d-2];
+			t:= fibo[d];
+		end;
+	t:= n;
+	j:= 0;
+	For i:= d downto 1 do
+		begin
+			If fibo[i] <= t then
+				begin
+					inc(j);
+					vt[j]:= i;
+					t:= t - fibo[i];
+				end;
+		end;
+		If t <> 0 then ok:= false;
+		If Ok = true then
+		begin 
+		write(g,n,'=');
+				for i:= 1 to j do
+					  begin
+					  If i <> j then
+					  write(g, fibo[vt[i]], '+') else
+					  write(g, fibo[vt[i]])
+					  end
+		end
+					  else write(g, 'Khong the bieu dien ');
 end;
 Begin
-   nhap;
-   Xuli;
-   close(f);
-   close(g);
+	nhap;
+	xuli;
+	close(f);
+	close(g);   
 end.
