06deacdf96a3ae8a8c7fffc184591cc8fc720a45
unknown <quandev2k7@gmail.com>
2022-06-14 18:34:31 +0700

    Pascal

--- abc.pas
+++ abc.pas
@@ -22,16 +22,16 @@ begin
 		s:=copy(s,2,n-1)+s[1];
 		inc(d);
 		a[d]:=s;
-		end;
-		for i:=1 to d-1 do 
-		for j:=i+1 to d do 
-		if a[i]>a[j] then 
-		begin 
+	end;
+	for i:=1 to d-1 do 
+	for j:=i+1 to d do 
+	if a[i]>a[j] then 
+	begin 
 		t:=a[i];
 		a[i]:=a[j];
 		a[j]:=t;
-		end;
-		write(g,a[k]);
+	end;
+	write(g,a[k]);
 end;
 begin 
 	nhap;
