422778b2895ee744870637ed4d2445536ac17ed0
unknown <quandev2k7@gmail.com>
2022-06-15 10:22:37 +0700

    Pascal

--- Bai1,4.pas
+++ Bai1,4.pas
@@ -15,11 +15,11 @@ begin
 	for i:=1 to length(s)-1 do
 	for j:=i+1 to length(s) do
 	if s[i]>s[j] then
-	begin
-		t:=s[i];
-		s[i]:=s[j];
-		s[j]:=t;
-	end;
+		begin
+			t:=s[i];
+			s[i]:=s[j];
+			s[j]:=t;
+		end;
 	for i:=2 to length(s) do
 	if s[i]<>s[i-1] then d:=d+1;
 	writeln(g,d);
